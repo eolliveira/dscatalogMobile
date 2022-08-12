10abd07c0cf0addcd3bc4d0647c0e3074604d298
Erick Oliveira <erick.oliveira9607@gmail.com>
2022-08-12 17:53:13 -0300

    teste

--- app/src/main/java/com/example/dscatalogmobile/gui/activity/CadastroProdutoActivity.kt
+++ app/src/main/java/com/example/dscatalogmobile/gui/activity/CadastroProdutoActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import com.example.dscatalogmobile.R
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
 
 class CadastroProdutoActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +15,13 @@ class CadastroProdutoActivity : BaseActivity() {
         setTitle("Cadastro de Produto")
 
 
+//        val items = listOf("Option 1", "Option 2", "Option 3", "Option 4")
+//        val adapter = ArrayAdapter(this, R.layout.list_item, items)
+//        (textField.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+
+
+        val items = arrayOf("Item 1", "Item 2", "Item 3", "Item 4")
+        (textField.editText as? MaterialAutoCompleteTextView)?.setSimpleItems(items)
 
     }
 }
